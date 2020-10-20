b75261039f8cbe021445b4074f9a7855995fd685
JDiaz <js.diazn@gmail.com>
2020-10-20 17:05:55 -0300

    initial

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,7 +1,4 @@
 import expressLoader from './express';
-import dependencyInjectorLoader from './dependencyInjector';
-import mongooseLoader from './mongoose';
-import jobsLoader from './jobs';
 import { Container } from 'typedi';
 import Logger from './logger';
 import { db } from '../db';
@@ -9,47 +6,17 @@ import { db } from '../db';
 import './events';
 
 export default async ({ expressApp }) => {
-
-  // const mongoConnection = await mongooseLoader();
-  // Logger.info('✌️ MongoDB loaded and connected!');
-
   Container.set('logger', Logger);
   try {
-    let data = await db.user.getAll();    
+    await db.user.findById(1);
     Logger.info('✌️ PostgreSQL loaded and connected!');
   } catch (error) {
     Logger.error('PostgreSQL error: %s', error.message || error);
     Logger.error(error);
   }
 
-
-  /**
-   * WTF is going on here?
-   *
-   * We are injecting the mongoose models into the DI container.
-   * I know this is controversial but will provide a lot of flexibility at the time
-   * of writing unit tests, just go and check how beautiful they are!
-   */
-  // const userModel = {
-  //   name: 'userModel',
-  //   // Notice the require syntax and the '.default'
-  //   model: require('../models/user').default,
-  // };
-
-  // It returns the agenda instance because it's needed in the subsequent loaders
-  // const { agenda } = await dependencyInjectorLoader({
-  //   mongoConnection,
-  //   models: [
-  //     userModel,
-  //     // salaryModel,
-  //     // whateverModel
-  //   ],
-  // });
-  // Logger.info('✌️ Dependency Injector loaded');
-
-  // await jobsLoader({ agenda });
-  // Logger.info('✌️ Jobs loaded');
-
-  await expressLoader({ app: expressApp });
+  expressLoader({ app: expressApp });
   Logger.info('✌️ Express loaded');
+  Logger.info('✌️ Environment: %s', process.env.NODE_ENV);
+
 };
